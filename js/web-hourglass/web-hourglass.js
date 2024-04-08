7249a4342f2d79f4cfb9ad6f677d60479ca3f2cf
Daniel López López <loplopez.daniel@gmail.com>
2024-04-08 17:18:32 +0200

    feat(#3): (#4)
    Added half time alert functionality

--- js/web-hourglass/web-hourglass.js
+++ js/web-hourglass/web-hourglass.js
@@ -23,12 +23,21 @@ window.onload = (event) => {
                     chrome.storage.sync.get("usedTimeSec", (usedTimeSec) => {
                         chrome.storage.sync.get("lastDay", (lastDay) => {
                             if (lastDay.lastDay != new Date().getDate()) {
-                                chrome.storage.sync.set({ usedTimeSec: 0, lastDay: new Date().getDate() })
+                                chrome.storage.sync.set({ usedTimeSec: 0, lastDay: new Date().getDate(), extraTime: false, halfTimeAlert: false})
                             }
                             chrome.storage.sync.set({ usedTimeSec: usedTimeSec.usedTimeSec + 5, lastDay: new Date().getDate() })
                             chrome.storage.sync.get("timeSec", (timeSec) => {
                                 if (timeSec.timeSec != 0 && usedTimeSec.usedTimeSec + 5 > timeSec.timeSec) {
                                     limitTime()
+                                } else if (timeSec.timeSec != 0 && timeSec.timeSec/(usedTimeSec.usedTimeSec + 5)<= 2 ) {
+                                    chrome.storage.sync.get("halfTimeAlert", (halfTimeAlert) => {
+                                        if (halfTimeAlert.halfTimeAlert) {
+                                            return;
+                                        } else {
+                                            window.alert("You have used 50% of your time!")
+                                            chrome.storage.sync.set({ halfTimeAlert: true });
+                                        }
+                                    });
                                 }
                             });
                         })
@@ -45,7 +54,7 @@ chrome.storage.sync.get("timeSec", (timeSec) => {
     chrome.storage.sync.get("usedTimeSec", (usedTimeSec) => {
         chrome.storage.sync.get("lastDay", (lastDay) => {
             if (lastDay.lastDay != new Date().getDate()) {
-                chrome.storage.sync.set({ usedTimeSec: 0, lastDay: new Date().getDate(), extraTime: false })
+                chrome.storage.sync.set({ usedTimeSec: 0, lastDay: new Date().getDate(), extraTime: false, halfTimeAlert: false })
                 return;
             }
             if (timeSec.timeSec == 0) {
